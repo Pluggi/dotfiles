d442d3eefff1763f2e7b764b69fc7d8b3a12b0a8
Antoine "Pluggi" Bardoux <pluggi512@gmail.com>
2014-10-17 18:30:26 +0200

    Add a basic installation script

---
+++ install.zsh
@@ -0,0 +1,23 @@
+#! /bin/zsh
+
+setopt extendedglob
+setopt glob_dots
+
+backup="$HOME/dotfiles_old"
+
+# Synchronize the git submodules
+git submodule init
+git submodule update
+
+# Create symlinks
+for f in *.sl
+do
+	new="$HOME/.${f/.sl/}"
+	if [[ -e "$new" ]]
+	then
+		mkdir -p backup
+		echo "$new alreay exists. Moving it to $backup"
+		mv "$new" "$backup"
+	fi
+	ln -vs "$PWD/$f" "$new"
+done
