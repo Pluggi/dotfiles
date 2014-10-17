d792aeb17946f3b546d70e64f318dcd4253d0bbd
Pluggi <pluggi512@gmail.com>
2014-10-17 22:04:24 +0200

    Retab file

--- install.zsh
+++ install.zsh
@@ -12,12 +12,12 @@ git submodule update
 # Create symlinks
 for f in *.sl
 do
-	new="$HOME/.${f/.sl/}"
-	if [[ -e "$new" ]]
-	then
-		mkdir -p backup
-		echo "$new alreay exists. Moving it to $backup"
-		mv "$new" "$backup"
-	fi
-	ln -vs "$PWD/$f" "$new"
+    new="$HOME/.${f/.sl/}"
+    if [[ -e "$new" ]]
+    then
+        mkdir -p backup
+        echo "$new alreay exists. Moving it to $backup"
+        mv "$new" "$backup"
+    fi
+    ln -vs "$PWD/$f" "$new"
 done
