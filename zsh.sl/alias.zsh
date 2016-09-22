1f973a618edea2514f1f3139e119e11f21b7fa60
Pluggi <pluggi512@gmail.com>
2016-09-22 22:03:15 +0200

    zsh: aliases: Add mkcd and cdtmp aliases

--- zsh.sl/alias.zsh
+++ zsh.sl/alias.zsh
@@ -17,5 +17,8 @@ alias gdb='gdb -quiet'
 
 alias sr='ssh -l root'
 
+alias mkcd='_(){ mkdir $1; cd $1; }; _'
+alias cdtmp='cd $(mktemp -d)'
+
 alias yt-audio="youtube-dl -f bestaudio"
 alias yt-video="youtube-dl -f 'bestvideo+bestaudio/bestvideo+bestaudio'"
